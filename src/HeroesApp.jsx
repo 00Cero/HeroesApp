d99f7a718b463abda576bc7dc6a84fd0aa8cb205
Zero0 <00zerocode@gmail.com>
2023-03-20 19:03:10 +0100

    Auth Provider created

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from './ui/components/Navbar'
 import 'animate.css'
+import { AuthProvider } from './auth/context/AuthProvider'
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <hr />
       <div className='container-lg'>
         <Outlet />
       </div>
-    </>
+    </AuthProvider>
   )
 }
